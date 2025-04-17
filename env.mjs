917df1558e437153944cd79f6b7804029af6748b
muradpm <abdulkadyr0v@yandex.ru>
2025-04-17 10:51:19 +0300

    update env

--- env.mjs
+++ env.mjs
@@ -5,6 +5,8 @@ export const env = createEnv({
   server: {
     OPENAI_API_KEY: z.string().min(1),
     JINA_API_KEY: z.string().min(1),
+    UPSTASH_REDIS_REST_URL: z.string().min(1),
+    UPSTASH_REDIS_REST_TOKEN: z.string().min(1),
   },
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string().min(1),
@@ -12,5 +14,7 @@ export const env = createEnv({
   runtimeEnv: {
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
     JINA_API_KEY: process.env.JINA_API_KEY,
+    UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
+    UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
   },
 });
